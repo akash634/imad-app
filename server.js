62678d6f0e73038a9db1894812799cd6c546a4f4
akash634 <36621166+akash634@users.noreply.github.com>
2018-02-20 05:04:48 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article one', function (req, res) {
+  res.send(path.join('article one requested and will be serve here'));
+});
+
+app.get('/article two', function (req, res) {
+  res.send(path.join('article two requested and will be serve here'));
+});
+
+app.get('/article three', function (req, res) {
+  res.send(path.join('article three requested and will be serve here'));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
