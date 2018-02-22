aa46d8a3a0ce0ecf949d468382175fbfbde9d70b
akash634 <36621166+akash634@users.noreply.github.com>
2018-02-22 06:40:05 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //change the text of var element div
 var element=document.getElementById('mittal');
 element.innerHTML='New value';
+
+var img= document.getElementById('my image');
+img.onclick= function(){
+    img.style.marginLeft= "180";
+}
